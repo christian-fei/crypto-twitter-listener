38924318deb78cf7ce40436ddeab356ae0e62b60
Christian Fei <crifei93@gmail.com>
2018-04-08 14:21:01 +0200

    calc lat lon

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const client = new Twitter({
   access_token_secret: process.env.npm_config_twitter_access_token_secret
 })
 
+const r2 = require('r2')
 const http = require('http')
 const serveStatic = require('serve-static')
 const serve = serveStatic('.', {'index': ['index.html']})
@@ -54,14 +55,35 @@ function writeSSE (data) {
 
 const stream = client.stream('statuses/filter', {track: '#bitcoin, #litecoin, #ethereum, #javascript'})
 
-stream.on('data', (event) => {
-  if (!event.retweeted_status || !event.retweeted_status.extended_tweet) return
-  // console.log(event.retweeted_status.extended_tweet.full_text)
-  console.log(JSON.stringify(event))
-  console.log('new tweet', event.text)
-  console.log('----------------------------------------------')
-  writeSSE(JSON.stringify(event))
-  // client.post('favorites/create', {id: event.id_str}, (error, response) => {
+stream.on('data', async (data) => {
+  if (!data.retweeted_status || !data.retweeted_status.extended_tweet) return
+
+  if (!data.retweeted_status || !data.retweeted_status.place || !data.retweeted_status.place.bounding_box || !data.retweeted_status.place.bounding_box.coordinates) {
+    console.log('no coordinates')
+    if (data.retweeted_status.user.location) {
+      const location = data.retweeted_status.user.location
+      console.log('has location', location)
+      let coordinates = await r2(`https://geocode.xyz/${location}?geoit=csv`).text
+      coordinates = coordinates.split(',').map(parseFloat)
+      if (coordinates.find(x => Number.isNaN(x))) { return console.log('failed to get coordinates') }
+      const lat = avgGeoCoordinatesLat(coordinates)
+      const lon = avgGeoCoordinatesLon(coordinates)
+      console.log('-> ', coordinates, lat, lon)
+      writeSSE(JSON.stringify({lat, lon}))
+    }
+  } else {
+    console.log('has coordinates')
+    const lat = avgTwitterCoordinatesLat(data.retweeted_status.place.bounding_box.coordinates[0])
+    const lon = avgTwitterCoordinatesLon(data.retweeted_status.place.bounding_box.coordinates[0])
+    console.log('twitter: lat, lon', lat, lon)
+    writeSSE(JSON.stringify({lat, lon}))
+  }
+  // console.log(data.retweeted_status.extended_tweet.full_text)
+  // console.log(JSON.stringify(data))
+  // console.log('new tweet', data.text)
+  // console.log('----------------------------------------------')
+  // writeSSE(JSON.stringify(data))
+  // client.post('favorites/create', {id: data.id_str}, (error, response) => {
   //   if (error) return console.error(`failed to like: ${error.message}`)
   //   console.log('Tweet ID: ' + response.id_str + ' Liked! - "' + response.text + '"')
   // })
@@ -70,3 +92,17 @@ stream.on('data', (event) => {
 stream.on('error', (error) => {
   throw error
 })
+
+function avgTwitterCoordinatesLat (coordinates) {
+  return coordinates.reduce((sum, [curr]) => sum + curr, 0) / coordinates.length
+}
+function avgTwitterCoordinatesLon (coordinates) {
+  return coordinates.reduce((sum, [_, curr]) => sum + curr, 0) / coordinates.length
+}
+
+function avgGeoCoordinatesLat (coordinates) {
+  return coordinates.reduce((sum, curr, i) => sum + (i % 2 === 0 ? curr : 0), 0) / coordinates.length / 2
+}
+function avgGeoCoordinatesLon (coordinates) {
+  return coordinates.reduce((sum, curr, i) => sum + (i % 2 === 1 ? curr : 0), 0) / coordinates.length / 2
+}
