36f459a7c98002a717e8651822075a4e7ad2ad1f
Christian Fei <crifei93@gmail.com>
2018-04-08 12:59:25 +0200

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+https://dev.to/notfakedev/twitter-bot-in-17-lines-of-code-100m?utm_source=digest_mailer&utm_medium=email&utm_campaign=digest_email
+
+## configure
+
+create an `.npmrc` with the following content (create an app @ [apps.twitter.com](https://apps.twitter.com))
+
+```
+twitter_consumer_key=
+twitter_consumer_secret=
+twitter_access_token_key=
+twitter_access_token_secret=
+```
+
+## start
+
+```
+npm start
+```
---
+++ index.js
@@ -0,0 +1,25 @@
+const Twitter = require('twitter')
+const client = new Twitter({
+  consumer_key: process.env.npm_config_twitter_consumer_key,
+  consumer_secret: process.env.npm_config_twitter_consumer_secret,
+  access_token_key: process.env.npm_config_twitter_access_token_key,
+  access_token_secret: process.env.npm_config_twitter_access_token_secret
+})
+
+const stream = client.stream('statuses/filter', {track: '#bitcoin, #litecoin, #ethereum'})
+
+stream.on('data', (event) => {
+  // console.log(Object.keys(event))
+  // console.log(JSON.stringify(event))
+  if (!event.retweeted_status || !event.retweeted_status.extended_tweet) return
+  console.log(event.retweeted_status.extended_tweet.full_text)
+  console.log('-----------------------------')
+  // client.post('favorites/create', {id:event.id_str}, (error, response) => {
+  //   if(error) throw error;
+  //   console.log('Tweet ID: '+response.id_str+' Liked! - "'+response.text+'"')
+  // });
+})
+
+stream.on('error', (error) => {
+  throw error
+})
